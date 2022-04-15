5dec6484b283abab2fee36c4fda5d4920bfe2971
JosephRodolfo <38168806+JosephRodolfo@users.noreply.github.com>
2022-04-15 19:28:38 -0400

    Add geocode function to get coordinates from location name

---
+++ sampleResponse.js
@@ -0,0 +1,39 @@
+const sample = {
+    request: {
+      type: 'LatLon',
+      query: 'Lat 37.83 and Lon -122.42',
+      language: 'en',
+      unit: 'm'
+    },
+    location: {
+      name: 'North Beach',
+      country: 'United States of America',
+      region: 'California',
+      lat: '37.806',
+      lon: '-122.411',
+      timezone_id: 'America/Los_Angeles',
+      localtime: '2022-04-15 09:25',
+      localtime_epoch: 1650014700,
+      utc_offset: '-7.0'
+    },
+    current: {
+      observation_time: '04:25 PM',
+      temperature: 11,
+      weather_code: 116,
+      weather_icons: [
+        'https://assets.weatherstack.com/images/wsymbols01_png_64/wsymbol_0002_sunny_intervals.png'
+      ],
+      weather_descriptions: [ 'Partly cloudy' ],
+      wind_speed: 11,
+      wind_degree: 270,
+      wind_dir: 'W',
+      pressure: 1018,
+      precip: 0,
+      humidity: 63,
+      cloudcover: 75,
+      feelslike: 10,
+      uv_index: 3,
+      visibility: 16,
+      is_day: 'yes'
+    }
+  }
