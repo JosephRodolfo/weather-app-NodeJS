4e5007f7ad77371c62018ca18055c3e0f4fb5de9
JosephRodolfo <38168806+JosephRodolfo@users.noreply.github.com>
2022-04-16 08:19:15 -0400

    Chain geocode and forecast together and add address input via process.argv command line

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-const geocode = require('./utils/geocode')
-const forecast = require('./utils/forecast')
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
 
-geocode("Boston", (error, data) => {
-  console.log("Error", error);
-  console.log("Data", data);
-});
+const address = process.argv[2];
+if(!address){
+  console.log('Please provide an address!');
 
-forecast(40.714, -74.006, (error, data) => {
-    console.log('Error', error);
-    console.log('Data', data);
-})
+} else {
+
+  geocode(address, (error, data) => {
+    if (error) {
+      return console.log(error);
+    }
+
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+      if (error) {
+        return console.log(error);
+      }
+
+      console.log(data.location);
+      console.log(forecastData);
+    });
+  });
+  console.log(address);
+
+
+}
